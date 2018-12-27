98bd9fe94552b02e3e65295970725cbe5b49b254
no.47 <jascal@163.com>
2018-12-27 22:03:03 +0800

    adjust toolbar state
    module: app
    
    on branch: master

---
+++ app/src/main/java/com/jascal/galatea/widget/ModelSwitcher.kt
@@ -0,0 +1,15 @@
+package com.jascal.galatea.widget
+
+import android.content.Context
+import android.view.View
+
+/**
+ * @author ihave4cat
+ * @describe TODO
+ * @data on 2018/12/27 10:00 PM
+ * @email jascal@163.com
+ * */
+
+class ModelSwitcher(context: Context?) : View(context) {
+
+}
